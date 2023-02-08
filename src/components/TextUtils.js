a28e06938c2e48ede984274cc76351dadb21851d
Uttkarsh Vedang <uttkarshaaa003@gmail.com>
2023-02-08 22:10:20 +0530

    initial commit

---
+++ src/components/TextUtils.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function TextUtils() {
+    return (
+        <>
+            <div className="container my-2">
+                <h1>Enter Text Here</h1>
+                <div class="mb-3">
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
+                </div>
+            </div>
+        </>
+    )
+}
